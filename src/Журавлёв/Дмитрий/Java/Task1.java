56cc83a553114be32ad64e4fb4b792d319cd9d02
zhyravlevdima <zhyravlevdima@yandex.com>
2022-11-13 12:50:58 +0300

    task1_final

--- src/Журавлёв/Дмитрий/Java/Task1.java
+++ src/Журавлёв/Дмитрий/Java/Task1.java
@@ -3,5 +3,32 @@ package Журавлёв.Дмитрий.Java;
 public class Task1 {
     public static void main(String[] args) {
 
+        System.out.println(isWinnerTicketByArray(123123));
+    }
+    public static boolean isWinnerTicketByArray(int numberOfTicket) {
+        int temp = numberOfTicket;
+        int counter = 0;
+        while (temp != 0) {
+            temp /= 10;
+            counter++;
+        }
+        if (counter < 2 || counter > 8 || counter % 2 != 0) {
+            return false;
+        }
+
+        int[] array = new int[counter];
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int index = 0; index < array.length; index++) {
+            array[index] = numberOfTicket % 10;
+            numberOfTicket /= 10;
+            if (index < array.length / 2) {
+                leftSum += array[index];
+            } else {
+                rightSum += array[index];
+            }
+        }
+        return leftSum == rightSum;
     }
 }
