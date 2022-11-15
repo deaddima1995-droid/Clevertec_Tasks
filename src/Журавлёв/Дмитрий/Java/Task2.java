9acb70731be7615a301d2ce7b8ddb75876f5e7f1
Дмитрий Журавлёв <zhyravlevdima@yandex.com>
2022-11-15 21:53:12 +0300

    update

--- src/Журавлёв/Дмитрий/Java/Task2.java
+++ src/Журавлёв/Дмитрий/Java/Task2.java
@@ -1,9 +1,16 @@
 package Журавлёв.Дмитрий.Java;
 
+import java.util.ArrayList;
+
 public class Task2 {
     public static void main(String[] args) {
 
     }
+
+}
+
+class AutoPark {
+    ArrayList<Car> cars = new ArrayList<>();
 
 }
 
@@ -33,27 +40,29 @@ abstract class Car {
     }
 }
 
-class Truck extends Car {
+class Cargo extends Car implements TransportCargo {
     private final int cargoVolume;
     private final int tonnage;
 
     private final Body body;
-    private final Cargo cargo;
+    private final CargoOfTruck cargoOfTruck;
 
-    public Truck(int year, Fuel fuel, Model model, Brand brand, int fuelFlow, int cargoVolume, int tonnage, Body body, Cargo cargo) {
+    public Cargo(int year, Fuel fuel, Model model, Brand brand, int fuelFlow, int cargoVolume, int tonnage, Body body, CargoOfTruck cargoOfTruck) {
         super(year, fuel, model, brand, fuelFlow);
         this.cargoVolume = cargoVolume;
         this.tonnage = tonnage;
         this.body = body;
-        this.cargo = cargo;
+        this.cargoOfTruck = cargoOfTruck;
     }
 
+    @Override
     public void sealCargo() {
 
     }
 }
 
-class Passenger extends Car {
+
+class Passenger extends Car implements TransportPassenger {
     private final int passengerCapacity;
 
     public Passenger(int year, Fuel fuel, Model model, Brand brand, int fuelFlow, int passengerCapacity) {
@@ -61,16 +70,46 @@ class Passenger extends Car {
         this.passengerCapacity = passengerCapacity;
     }
 
+    @Override
     public void disinfectSalon() {
 
     }
 }
 
+class CargoAndPassenger extends Car implements TransportCargo, TransportPassenger {
+    private final int cargoVolume;
+    private final int tonnage;
+    private final int passengerCapacity;
+
+    public CargoAndPassenger(int year, Fuel fuel, Model model, Brand brand, int fuelFlow, int cargoVolume, int tonnage, int passengerCapacity) {
+        super(year, fuel, model, brand, fuelFlow);
+        this.cargoVolume = cargoVolume;
+        this.tonnage = tonnage;
+        this.passengerCapacity = passengerCapacity;
+    }
+
+     @Override
+     public void sealCargo() {
+
+     }
+
+     @Override
+     public void disinfectSalon() {
+
+     }
+ }
+
 interface Body {
 
 }
+interface TransportCargo {
+    public void sealCargo();
+}
+interface TransportPassenger {
+    public void disinfectSalon();
+}
 
-enum Cargo {
+enum CargoOfTruck {
     manufacturedGoods;
 }
 
